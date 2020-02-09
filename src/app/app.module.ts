fd93056bffd5aec484365c241c9a16196b458db4
cmunhozprog02h <cmunhozprog02@hotmail.com>
2020-02-09 09:17:30 -0300

    Myfirst

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import{ MyFirstComponent} from './myfirst.component';
+import { MysecondComponent } from './mysecond/mysecond.component';
+import { MyThirdComponent } from './my-second/my-third/my-third.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponent,
+    MysecondComponent,
+    MyThirdComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/myfirst.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'mysfirst',
+    templateUrl: './myfirst.component.html',
+    styleUrls: ['./myfirst.component.css']
+       
+   
+})
+export class MyFirstComponent {
+
+}
